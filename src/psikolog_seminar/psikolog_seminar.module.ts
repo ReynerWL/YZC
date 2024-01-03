a623f8de448057f2b7e0e2145e621c2bda705a39
Nazhwa <nazhwanursyabrina@gmail.com>
2024-01-03 10:59:58 +0700

    commit isinya gambar semua

--- src/psikolog_seminar/psikolog_seminar.module.ts
+++ src/psikolog_seminar/psikolog_seminar.module.ts
@@ -4,8 +4,8 @@ import { PsikologSeminarController } from './psikolog_seminar.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PsikologSeminar } from './entities/psikolog_seminar.entity';
 import { PsikologModule } from '#/psikolog/psikolog.module';
-import { SeminarModule } from '#/seminar/seminar.module';
-import { SeminarService } from '#/seminar/seminar.service';
+// import { SeminarModule } from '#/seminar/seminar.module';
+// import { SeminarService } from '#/seminar/seminar.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([PsikologSeminar]), PsikologModule],
